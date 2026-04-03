8c0b8c48b43115fbd15a564d7580e2d2df85df44
Janani M <your-github-email@gmail.com>
2026-04-03 18:36:23 +0530

    Initial commit: full-stack AI Pass/Fail Predictor

---
+++ database.py
@@ -0,0 +1,17 @@
+import mysql.connector
+from mysql.connector import Error
+
+def get_connection():
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="Janupriya1820",
+            database="ai_microservice"
+        )
+
+        return connection
+
+    except Error as e:
+        print("Error while connecting to MySQL:", e)
+        return None
